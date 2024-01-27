cd1f870939557ca9593c9ffa2c68a0ce29eeb0f8
SpyderCoder <100071262+SpyderCoder@users.noreply.github.com>
2024-01-27 00:37:55 -0700

    Add timer based movement

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,28 +1,51 @@
 extends CharacterBody2D
 
+const MAX_SPEED = 350.0
+const MAX_JUMP_VELOCITY = -400.0
+const SPEED_MULTIPLIER = 5
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var accel_timer = $AccelerateTimer
+var speed : float
+var jump_velocity : float
+var last_direction : int
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
 	# Add the gravity.
-	#if not is_on_floor():
-		#velocity.y += gravity * delta
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = speed
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
+	if(direction != 0):
+		last_direction = direction
 	if direction:
-		velocity.x = direction * SPEED
+		if(speed < MAX_SPEED and accel_timer.is_stopped()):
+			_on_accelerate_timer_timeout(direction)
+		velocity.x = direction * speed
+		if Input.is_action_just_pressed("slide") and is_on_floor():
+			velocity.x = direction * speed * 3.0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if(speed > 0):
+			_on_accelerate_timer_timeout(0)
+			velocity.x = last_direction * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+
+
+func _on_accelerate_timer_timeout(direction):
+	if(direction != 0):
+		speed += SPEED_MULTIPLIER
+	else:
+		speed -= SPEED_MULTIPLIER
+	return speed
