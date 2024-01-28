92d7fd29365a8443aae82135d3bd3b6e09a65bd9
Taylor Nielsen <littleb88888888@gmail.com>
2024-01-27 22:13:15 -0700

    Added vertical tracking to camera

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -2,8 +2,14 @@ class_name CameraController
 extends Camera2D
 
 @export var player: CharacterBody2D
+@export var follow_offset := Vector2(0, -12)
+@export var y_lerp := 0.2
 
 func _process(_delta):
-	global_position.x = player.global_position.x
+	var target_position = player.global_position + follow_offset
+	
+	global_position.x = target_position.x
 	if global_position.x < 0:
 		global_position.x = 0
+	
+	global_position.y = lerp(global_position.y, target_position.y, y_lerp)
