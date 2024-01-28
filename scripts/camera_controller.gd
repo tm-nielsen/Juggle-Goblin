796758610ff51658107e29fb844e83276797c648
Taylor Nielsen <littleb88888888@gmail.com>
2024-01-28 00:58:04 -0700

    Added slope offset to camera controller

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -3,7 +3,8 @@ extends Camera2D
 
 @export var player: CharacterBody2D
 @export var follow_offset := Vector2(0, -12)
-@export var y_lerp := 0.2
+@export_range(0, 1) var y_lerp := 0.2
+@export var slope_offset_factor := 10.0
 
 func _process(_delta):
 	var target_position = player.global_position + follow_offset
@@ -11,5 +12,15 @@ func _process(_delta):
 	global_position.x = target_position.x
 	if global_position.x < 0:
 		global_position.x = 0
+		
+	if player.is_on_floor():
+		var slope_offset = _get_slope_offset()
+		global_position.y -= slope_offset
+		global_position.y = lerp(global_position.y, target_position.y, y_lerp)
+		global_position.y += slope_offset
+		
+
+func _get_slope_offset() -> float:
+	var floor_normal = player.get_floor_normal()
+	return slope_offset_factor * floor_normal.x
 	
-	global_position.y = lerp(global_position.y, target_position.y, y_lerp)
