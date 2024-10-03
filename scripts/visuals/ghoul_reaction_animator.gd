31b7c962b33e950dd6fc2b61d3b162055d71492f
Twig Nielsen <littleb88888888@gmail.com>
2024-10-03 13:52:10 -0600

    Added ghoul appear/disappear animations

--- scripts/visuals/ghoul_reaction_animator.gd
+++ scripts/visuals/ghoul_reaction_animator.gd
@@ -1,7 +1,15 @@
 extends AnimatedSprite2D
 
-@export var responses_enabled: bool = false
+enum PresenceState {ABSENT, APPEARING, PRESENT, DISAPPEARING}
+const ABSENT = PresenceState.ABSENT
+const APPEARING = PresenceState.APPEARING
+const PRESENT = PresenceState.PRESENT
+const DISAPPEARING = PresenceState.DISAPPEARING
 
+@export var enable_responses: bool = true
+@export var appear_delay: float = 2
+
+@export_subgroup('laugh parameters')
 @export var base_laugh_chance: float = 0.05
 @export var dropped_laugh_buildup: float = 0.1
 @export var died_laugh_buildup: float = 0.25
@@ -11,12 +19,18 @@ extends AnimatedSprite2D
 
 @onready var laugh_chance := base_laugh_chance
 
+var responses_enabled: bool: get = _get_responses_enabled
+var state: PresenceState = ABSENT
+
 
 func _ready():
+  hide()
+  LevelSignalBus.level_started.connect(_on_level_started)
+  LevelSignalBus.level_completed.connect(_on_level_completed)
+  _on_level_started()
   LevelSignalBus.ball_dropped.connect(_on_ball_dropped)
   LevelSignalBus.player_died.connect(_on_player_died)
   LevelSignalBus.new_checkpoint_reached.connect(_on_checkpoint_reached)
-  LevelSignalBus.level_completed.connect(_on_level_completed)
   animation_finished.connect(_on_animation_finished)
 
 
@@ -28,6 +42,21 @@ func trigger_laugh_chance(laugh_buildup: float):
     laugh_chance = base_laugh_chance
   else:
     play("Smirk")
+
+
+func _on_level_started():
+  var delay_tween = create_tween()
+  delay_tween.tween_interval(appear_delay)
+  delay_tween.tween_callback(_appear)
+
+func _appear():
+  state = APPEARING
+  play('Appear')
+  show()
+
+func _on_level_completed():
+  state = DISAPPEARING
+  play('Disappear')
 
 
 func _on_ball_dropped():
@@ -43,12 +72,16 @@ func _on_checkpoint_reached():
     play("Wheeze")
     laugh_chance = base_laugh_chance
 
-func _on_level_completed():
-  pass
-  # responses_enabled = false
-  # hide()
-
 
 func _on_animation_finished():
-  if animation != "Idle":
+  if state == APPEARING:
+    state = PRESENT
+  elif state == DISAPPEARING:
+    state = ABSENT
+    hide()
+  elif animation != "Idle":
     play("Idle")
+
+
+func _get_responses_enabled() -> bool:
+  return enable_responses && state == PRESENT
