e61a8fae6f1630ab7d00aa3ae6c3d5c0c1eadde3
Twig Nielsen <littleb88888888@gmail.com>
2024-10-04 00:49:14 -0600

    Fixed level background - made camera tracking position smoothing manual to make background follow properly

--- scripts/level/camera_controller.gd
+++ scripts/level/camera_controller.gd
@@ -3,6 +3,8 @@ extends Camera2D
 
 @export var player: CharacterBody2D
 @export var follow_offset := Vector2(0, -12)
+@export var follow_strength := 0.1
+@export var maximum_follow_speed := 10.0
 @export_range(0, 1) var y_lerp := 0.2
 @export var slope_offset_factor := 10.0
 
@@ -18,20 +20,31 @@ func _ready():
 	LevelSignalBus.ball_dropped.connect(_start_death_freeze)
 	LevelSignalBus.player_died.connect(_start_death_freeze)
 
-func _process(_delta):
+func _process(delta):
 	if tracking_enabled:
 		var total_offset = follow_offset + area_offset
 		target_position = player.global_position + total_offset
 	
-	global_position.x = target_position.x
+	_track_horizontally(delta)
+	
+	if player.is_on_floor():
+		_track_vertically()
+
+
+func _track_horizontally(delta):
+	var displacement = target_position.x - global_position.x
+	displacement *= follow_strength * delta * 60
+	displacement = clampf(displacement, -maximum_follow_speed, maximum_follow_speed)
+	global_position.x += displacement
 	if global_position.x < 0:
 		global_position.x = 0
-		
-	if player.is_on_floor():
-		var slope_offset = _get_slope_offset()
-		global_position.y -= slope_offset
-		global_position.y = lerp(global_position.y, target_position.y, y_lerp)
-		global_position.y += slope_offset
+
+
+func _track_vertically():
+	var slope_offset = _get_slope_offset()
+	global_position.y -= slope_offset
+	global_position.y = lerp(global_position.y, target_position.y, y_lerp)
+	global_position.y += slope_offset
 		
 
 func _get_slope_offset() -> float:
@@ -47,4 +60,4 @@ func _start_death_freeze():
 
 func _enable_tracking():
 	tracking_enabled = true
-	global_position = player.global_position + follow_offset
+	# global_position = player.global_position + follow_offset
