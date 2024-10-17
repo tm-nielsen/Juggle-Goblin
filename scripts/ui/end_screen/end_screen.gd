0adf11f39ae14219613207494450d3df7fb99cb1
Twig Nielsen <littleb88888888@gmail.com>
2024-10-17 11:04:44 -0600

    Added delay before enabling game reset - added delay to end screen leaderboard display before enabling reset - refactored flashing display to be externally triggered - adjusted end screen node tree to work with both hidden and shown reset prompt

--- scripts/ui/end_screen/end_screen.gd
+++ scripts/ui/end_screen/end_screen.gd
@@ -2,13 +2,20 @@ extends Control
 
 signal reset_triggered
 
-enum DisplayState {HIDDEN, NAME_ENTRY, LEADERBOARD}
+enum DisplayState {HIDDEN, NAME_ENTRY, LEADERBOARD, RESET_ENABLED}
+const HIDDEN = DisplayState.HIDDEN
+const NAME_ENTRY = DisplayState.NAME_ENTRY
+const LEADERBOARD = DisplayState.LEADERBOARD
+const RESET_ENABLED = DisplayState.RESET_ENABLED
 
 @export var initial_display: Control
 @export var final_display: Control
 
 @export var completion_time_label: TimeLabel
 @export var name_entry_area: NameEntryArea
+
+@export var reset_prompt: FlashingDisplay
+@export var reset_delay: float = 3
 
 var display_state: DisplayState
 
@@ -20,24 +27,23 @@ var final_death_count: int
 
 
 func _ready():
+  display_state = HIDDEN
   hide()
-  display_state = DisplayState.HIDDEN
   name_filter = NameFilter.new()
   name_entry_area.name_changed.connect(_on_name_changed)
   name_entry_area.name_confirmed.connect(_on_name_confirmed)
 
 func _process(_delta: float):
-  if display_state == DisplayState.LEADERBOARD && \
-      Input.is_action_just_pressed('ui_accept'):
+  if display_state == RESET_ENABLED && Input.is_action_just_pressed('ui_accept'):
     StatTracker.reset()
-    display_state = DisplayState.HIDDEN
+    display_state = HIDDEN
     hide()
     reset_triggered.emit()
 
 
 func start_display():
   show()
-  display_state = DisplayState.NAME_ENTRY
+  display_state = NAME_ENTRY
   initial_display.show()
   final_display.hide()
 
@@ -54,6 +60,17 @@ func _on_name_changed(new_name: String):
 
 func _on_name_confirmed(confirmed_name: String):
   CompletionStatsIO.append_completion(confirmed_name, completion_time, final_death_count)
-  display_state = DisplayState.LEADERBOARD
+  display_state = LEADERBOARD
   initial_display.hide()
   final_display.show()
+  _enable_reset_after_delay()
+
+
+func _enable_reset_after_delay():
+  var delay_tween = create_tween()
+  delay_tween.tween_interval(reset_delay)
+  delay_tween.tween_callback(_enable_reset)
+
+func _enable_reset():
+  reset_prompt.start_flashing()
+  display_state = RESET_ENABLED
--- scripts/visuals/flashing_display.gd
+++ scripts/visuals/flashing_display.gd
@@ -1,16 +1,29 @@
+class_name FlashingDisplay
 extends CanvasItem
 
 @export var on_period: float = 0.6
 @export var off_period: float = 0.4
+@export var flash_on_start: bool = false
+
+var flash_tween: Tween
 
 
 func _ready():
-  var flash_tween = create_tween()
+  hide()
+  if flash_on_start:
+    start_flashing()
+
+func start_flashing():
+  show()
+  flash_tween = create_tween()
   flash_tween.tween_callback(_set_colour.bind(Color.WHITE))
   flash_tween.tween_interval(on_period)
   flash_tween.tween_callback(_set_colour.bind(Color.TRANSPARENT))
   flash_tween.tween_interval(off_period)
   flash_tween.set_loops()
+
+func stop_flashing():
+  flash_tween.kill()
 
 func _set_colour(colour: Color):
   modulate = colour
