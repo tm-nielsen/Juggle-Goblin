f591adbf2b788755a04cb9991a61987a7f4a3ed7
Twig <littleb88888888@gmail.com>
2025-06-29 12:37:33 -0600

    Made background arrow point to checkpoint

--- scripts/tutorial/pin_indicator.gd
+++ scripts/tutorial/pin_indicator.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 @export var display_threshold: float = 50
 @export var checkpoint_manager: CheckpointManager
+@export var pointer_node: Node2D
 
 @export var bounce_amplitude: float = 8
 @export var bounce_frequency: float = 1
 
 var player: PlayerController
+var checkpoint_position: Vector2: get=_get_checkpoint_position
 var enabled: bool
 
 var base_position: float
@@ -25,21 +27,28 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
     if !enabled: return
-    visible = _get_checkpoint_offset() > display_threshold
+    visible = player.position.x - checkpoint_position.x > display_threshold
     _update_bounce_position(delta);
+    _update_rotation()
 
-func _get_checkpoint_offset() -> float:
-    return player.position.x - checkpoint_manager.active_checkpoint_position.x
+
+func set_active(active := true) -> void:
+    enabled = active
+    hide()
 
 func reset_bounce() -> void:
     position.y = base_position
     bounce_timer = 0
+
 
 func _update_bounce_position(delta: float) -> void:
     bounce_timer += delta
     var t = abs(cos(bounce_timer * bounce_frequency * TAU))
     position.y = base_position - bounce_amplitude * t
 
-func set_active(active := true) -> void:
-    enabled = active
-    hide()
+func _update_rotation() -> void:
+    pointer_node.rotation = global_position.angle_to_point(checkpoint_position)
+
+
+func _get_checkpoint_position() -> Vector2:
+    return checkpoint_manager.active_checkpoint_position
